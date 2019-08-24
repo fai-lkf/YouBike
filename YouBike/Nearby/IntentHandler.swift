bedc4d4d650e081289e845782d443224ae3352ad
Ricky Lau <laukimfairicky@gmail.com>
2019-08-24 10:51:49 +0800

    <WIP>

--- YouBike/Nearby/IntentHandler.swift
+++ YouBike/Nearby/IntentHandler.swift
@@ -11,10 +11,19 @@ import Intents
 class IntentHandler: INExtension {
     
     override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
-        
-        return self
+        switch intent {
+        case is NearbyParkIntent:
+            return NearbyParkIntentHandler()
+        default:
+            return self
+        }
     }
     
+}
+
+class NearbyParkIntentHandler: NSObject, NearbyParkIntentHandling  {
+    
+    func handle(intent: NearbyParkIntent, completion: @escaping (NearbyParkIntentResponse) -> Void) {
+        
+    }
 }
